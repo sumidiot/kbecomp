825c8d51787eee6f9eb622fd7915e9eab13fe994
Nick Hamblet <nick.hamblet@gmail.com>
2017-11-24 23:14:33 -0500

    Adam optimizer hooked up

--- transe.py
+++ transe.py
@@ -19,23 +19,35 @@ for lineRaw in f:
 f.close()
 
 triples = np.array(triples)
-h = torch.LongTensor(triples[:,0])
-r = torch.LongTensor(triples[:,1])
-t = torch.LongTensor(triples[:,2])
+h = Variable(torch.LongTensor(triples[:,0]), requires_grad=False)
+r = Variable(torch.LongTensor(triples[:,1]), requires_grad=False)
+t = Variable(torch.LongTensor(triples[:,2]), requires_grad=False)
 
 tn = np.random.randint(len(entIdx), size=len(triples))
-tNeg = torch.LongTensor(tn)
+tNeg = Variable(torch.LongTensor(tn), requires_grad=False)
 
 D = 50
 
 ent = torch.nn.Embedding(len(entIdx), D)
 rel = torch.nn.Embedding(len(relIdx), D)
 
-hpr = ent(h) + rel(r)
-tt = ent(Variable(t))
-ft = ent(Variable(tNeg)) # if this isn't a Variable, torch vomits, not entirely sure why
-pos = (hpr - tt).norm(dim=1)
-neg = (hpr - ft).norm(dim=1)
-marginLoss = torch.nn.MarginRankingLoss(margin=1)
-print marginLoss(pos, neg, Variable(torch.ones(len(triples))))
+e_lr = 1e-2
+r_lr = 1e-4
 
+e_optimizer = torch.optim.Adam(ent.parameters(), e_lr)
+r_optimizer = torch.optim.Adam(rel.parameters(), r_lr)
+
+for epoch in range(100):
+	hpr = ent(h) + rel(r)
+	tt = ent(t)
+	ft = ent(tNeg)
+	pos = (hpr - tt).norm(dim=1)
+	neg = (hpr - ft).norm(dim=1)
+	marginLoss = torch.nn.MarginRankingLoss(margin=1)
+	loss = marginLoss(pos, neg, Variable(torch.ones(len(triples))))
+	print loss
+        e_optimizer.zero_grad()
+        r_optimizer.zero_grad()
+	loss.backward()
+        e_optimizer.step()
+	r_optimizer.step()
